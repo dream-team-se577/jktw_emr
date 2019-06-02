527183a108d1ef6798719a7298ab9b7530955511
Oyesanmi <taiwo.oyesanmi@accenture.com>
2019-06-02 18:18:03 -0400

    ui updates - ongoing work

--- emr-ui/src/app/model/patient.ts
+++ emr-ui/src/app/model/patient.ts
@@ -1,7 +1,7 @@
 export class Patient {
 	id: number;
 	ssn: string;
-	first_name: string;
-	middle_name: string;
-	last_name: string;
+	firstName: string;
+	middleName: string;
+	lastName: string;
 }
